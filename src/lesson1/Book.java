8ba091d218034ff551f7abe57a5b2d1c86fca0d7
Shamsutdinov Vladislav <okomsky@yandex.ru>
2023-03-30 20:15:30 +1100

    добавил поле Author в класс Book

--- src/lesson1/Book.java
+++ src/lesson1/Book.java
@@ -3,10 +3,12 @@ package lesson1;
 public class Book {
      private String nameOfTheBook;
      private int ageOfPublication;
+     private class Author {}
 
      public Book(String nameOfTheBook, int ageOfPublication) {
           this.nameOfTheBook = nameOfTheBook;
           this.ageOfPublication = ageOfPublication;
+
      }
 
      public String getNameOfTheBook() {
